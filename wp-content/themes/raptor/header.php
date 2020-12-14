45c6857e737fd1aff4b6c7c9da6fa4a5480ae65d
Nikolai Kasperovich <nikolai@raisetech.net>
2020-12-14 12:44:50 +0300

    css fix

--- wp-content/themes/raptor/header.php
+++ wp-content/themes/raptor/header.php
@@ -34,6 +34,6 @@
 <?php if( $short_code = get_field('hello_bar_mattresses', get_the_ID()) ) : ?>
     <?= do_shortcode($short_code); ?>
 <?php endif; ?>
-<div class="app-wrapper main">
+<div class="app-wrapper <?php echo get_page_template_slug() === 'promotions-new.php' ? 'main' : '';?> ">
 
 <?php get_template_part( 'header-menu'); ?>
