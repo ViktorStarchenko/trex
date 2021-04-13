b0c09c42384ec3364c7ba9fc1be2de16edb425e8
N.Kasperovich <nikolai@raisetech.net>
2021-04-13 13:13:45 +0300

    debug

--- wp-content/themes/raptor/front-page.php
+++ wp-content/themes/raptor/front-page.php
@@ -254,9 +254,9 @@ if (empty($yotpo_id)) {
                 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
                 $server_output = curl_exec($ch);
-               /* $response = json_decode($server_output,true);
+                $response = json_decode($server_output,true);
 
-                $reviews[] = $response['response']['review'];*/
+                //$reviews[] = $response['response']['review'];*/
                 curl_close ($ch);
        //     }
         }
